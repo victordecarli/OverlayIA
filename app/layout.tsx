5ae9d0b0d76419b94dfd4061772f05dfdf5cd204
nagavineerpasam <nagavineerpasam@gmail.com>
2024-12-24 21:56:30 +0530

    added analyticss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import { inter } from './fonts';
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: 'UnderlayX AI - Text and Shapes behind Images',
@@ -17,6 +19,8 @@ export default function RootLayout({
       <body className="min-h-screen bg-[#0A0A0A]">
         {children}
         <Toaster />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
