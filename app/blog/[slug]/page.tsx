9ab75ee8f531f50f8b3fcc863a4f5f8e6ac76e02
nagavineerpasam <nagavineerpasam@gmail.com>
2025-01-03 15:18:06 +0530

    seo changes

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,21 +1,25 @@
-import { Metadata } from 'next';
+import { Metadata, ResolvingMetadata } from 'next';
 import { BlogPost as BlogPostComponent } from '@/components/BlogPost';
 import { getBlogPost } from '@/lib/blog';
 
 type Props = {
-  params: { slug: string }
+  params: Promise<{ slug: string }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const post = await getBlogPost(params.slug);
-  
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const slug = (await params).slug;
+  const post = await getBlogPost(slug);
+
   return {
     title: `${post.title} | UnderlayX Blog`,
     description: post.description,
     openGraph: {
       title: post.title,
       description: post.description,
-      images: [post.image],
       type: 'article',
       publishedTime: post.date,
       authors: ['UnderlayX Team'],
@@ -24,14 +28,14 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     twitter: {
       card: 'summary_large_image',
       title: post.title,
-      description: post.description,
-      images: [post.image]
+      description: post.description
     }
   };
 }
 
 export default async function BlogPost({ params }: Props) {
-  const post = await getBlogPost(params.slug);
+  const slug = (await params).slug;
+  const post = await getBlogPost(slug);
   
   return <BlogPostComponent post={post} />;
 }
